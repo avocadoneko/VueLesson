344222a6bc377194073b8de8efa9a5a8cbe32a52
Natsuho Kimura <kimnatsu0815@gmail.com>
2018-10-25 19:06:13 +0900

    SVG samples

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 var app = new Vue ({
   el: '#app',
   data: {
-    message: '初期メッセージ',
-    list: ['apple', 'banana', 'orange'],
-    show: true
-  },
-  methods: {
-    handleClick: function(event) {
-      alert(event.target)
-    }
+    active: false
   }
 })
