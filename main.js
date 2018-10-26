c6f53cd287c7991359d506415beb74863404afa8
kimura-natsuho <kimura-natsuho@ryukyu-i.co.jp>
2018-10-26 18:57:05 +0900

    axios？

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-var app = new Vue ({
-  el: '#app',
-  data: {
-    name: 'キマイラ',
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 300 },
-      { id: 3, name: 'トロイ', hp: 2 }
-    ]
-  },
-  methods: {
-    doAdd: function() {
-      var max = this.list.reduce(function(a, b) {
-        return a.id > b.id ? a.id : b.id
-      }, 0)
+    new Vue ({
+      el: '#app',
+      data: {
+        list: []
+      },
+      created: function() {
+        axios.get('list.json').then(function(response) {
+          this.list = response.data
+        }.bind(this)).catch(function(e) {
+          console.error(e)
+        })
+        }
+      }
+    })
 
-      this.list.push({
-        id: max + 1,
-        name: this.name,
-        hp: 500
-      })
-    },
-
-    doRemove: function(index) {
-      this.list.splice(index, 1)
-    }
   }
 })
