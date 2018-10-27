471c832124ecbb33af4a055e9821a66a0e18294f
Natsuho Kimura <kimnatsu0815@gmail.com>
2018-10-27 16:56:46 +0900

    chapter2

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 new Vue({
   el: '#app',
   data: {
-    // あらかじめ空リストを用意しておく
-    list: []
-  },
-  created: function () {
-    axios.get('http://www.geocities.jp/tigre0815n/list.json').then(function (response) {
-      // 取得完了したらlistリストに代入
-      this.list = response.data
-    }.bind(this)).catch(function (e) {
-      console.error(e)
-    })
+    message: 'Helllo <strong>Vue.js!!!</strong>'
   }
 })
