e9719fb6ab5c475a71b20d5100972c9db08648ef
Natsuho Kimura <kimnatsu0815@gmail.com>
2018-10-27 15:24:35 +0900

    axiosできた

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-    new Vue ({
-      el: '#app',
-      data: {
-        list: []
-      },
-      created: function() {
-        axios.get('list.json').then(function(response) {
-          this.list = response.data
-        }.bind(this)).catch(function(e) {
-          console.error(e)
-        })
-        }
-      }
+new Vue({
+  el: '#app',
+  data: {
+    // あらかじめ空リストを用意しておく
+    list: []
+  },
+  created: function () {
+    axios.get('http://www.geocities.jp/tigre0815n/list.json').then(function (response) {
+      // 取得完了したらlistリストに代入
+      this.list = response.data
+    }.bind(this)).catch(function (e) {
+      console.error(e)
     })
-
   }
 })
