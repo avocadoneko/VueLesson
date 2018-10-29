9a6b1be5987069c9c7b0f45d951112b11dc8a27e
Natsuho Kimura <kimnatsu0815@gmail.com>
2018-10-29 22:32:09 +0900

    sort

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 new Vue({
   el: '#app',
   data: {
-    width: 800,
-    height: 600
+    budget: 300,
+    limit: 2,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 300 },
+      { id: 4, name: 'おれんじ', price: 400 },
+      { id: 5, name: 'めろん', price: 500 }
+    ],
+    order: false
   },
   computed: {
-    halfWidth: {
-      get: function() { return this.width / 2 },
-      set: function (val) { this.width = val * 2 },
+    matched: function() {
+      return this.list.filter(function(el) {
+        return el.price <= this.budget
+      }, this)
+    },
+    sorted: function() {
+      return _.orderBy(this.matched, 'price', this.order ? 'desc' : 'asc')
+    },
+    limited: function() {
+      return this.sorted.slice(0, this.limit)
     }
   }
 })
