72b5ba57320fb1040a60d16e3a52e8ccdf174e70
kimura-natsuho <kimura-natsuho@ryukyu-i.co.jp>
2018-10-30 17:35:23 +0900

    aaa

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
+Vue.component('comp-child', {
+  template: '<li>{{ name }} HP.{{ hp }}</li>',
+  props: ['name', 'hp']
+})
+
 new Vue({
   el: '#app',
   data: {
-    list: [],
-    current: '',
-    topics: [
-      { value: 'vue', name: 'Vue.js' },
-      { value: 'jQuery', name: 'jQuery' }
+    list: [
+      { id: 1, name: 'スライム', hp: 100},
+      { id: 2, name: 'ゴブリン', hp: 200},
+      { id: 3, name: 'ドラゴン', hp: 500}
     ]
-  },
-  watch: {
-    current: function(val) {
-      axios.get('https://api.github.com/search/repositories', {
-        params: { q: 'topic:' + val }
-      }).then(function(response) {
-        this.list = response.data.items
-      }.bind(this))
-    }
-  },
+  }
 })
